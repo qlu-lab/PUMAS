c89ad61d360e209ba4bfbc1ef065d2111137bd51
qlu-lab <33715415+qlu-lab@users.noreply.github.com>
2019-10-16 01:46:23 -0500

    Update prs_tuning.R

--- PRS-Fine-tuning/R/prs_tuning.R
+++ PRS-Fine-tuning/R/prs_tuning.R
@@ -1,8 +1,7 @@
-#source("/Users/yupeilin/PRS-Fine-tuning/R/CVFunII_v4.R")
 set.seed(0)
 
 ##################################################
-data.real <- read.table(paste0("/Users/yupeilin/PRS-Fine-tuning/input/T0030_pruned.txt"), header=T,na.strings=c("na","NA",""))
+data.real <- read.table(paste0("/PRS-Fine-tuning/PRS-Fine-tuning/input/T0030_pruned.txt"), header=T,na.strings=c("na","NA",""))
 data.real=na.omit(data.real)
 
 beta=data.real$Beta
@@ -22,7 +21,7 @@ Res_TildeRL <- FunII.TildeRL(FunII.beta=beta,FunII.SE=se,FunII.sigma=X_VAR,FunII
 #p-value selection
 Res_TildeRL=Res_TildeRL[-index.cons]
 
-png(paste0("/Users/yupeilin/PRS-Fine-tuning/result/T0030_pruned.png"),units="in",width = 9,height = 5,res = 300)
+png(paste0("/PRS-Fine-tuning/PRS-Fine-tuning/result/T0030_pruned.png"),units="in",width = 9,height = 5,res = 300)
 plot(y=Res_TildeRL,x=log(p.value,base=10),main=paste0("T0030_pruned"),ylab="COR^2",xlab="log(P-value)")
 points(y=max(Res_TildeRL),x=log(p.value,base=10)[which(Res_TildeRL==max(Res_TildeRL))],col="red", pch=19)
 abline(v=log(p.value,base=10)[which(Res_TildeRL==max(Res_TildeRL))],col="red",lty=2)
@@ -37,14 +36,6 @@ dev.off()
 #------ the paramters should be ordered in advance
 FunI.PEhat<-function(FunI.beta_hat,FunI.test.Y,FunI.test.X){
   if (! is.vector(FunI.beta_hat)){stop(print("beta_hat should be a vector!"))}
-  #if (! is.vector(FunI.XtY)){stop(print("X_t_Y should be a vector!"))}
-  #if (! (is.matrix(FunI.XtX) | is.data.frame(FunI.XtX))){stop(print("X_t_X should be a Matrix!"))}
-  #if (length(FunI.beta_hat)!=length(FunI.XtY) | length(FunI.beta_hat)!=nrow(FunI.XtX) | nrow(FunI.XtX)!=ncol(FunI.XtX)){stop(print("Please check dimensions of parameters!"))}
-  #if (! isSymmetric(FunI.XtX)){stop(print("X_t_X is not symmetric!"))}
-  #FunI.PE.term2 <- -2*FunI.beta_hat*FunI.XtY
-  #FunI.PE.term3 <- apply(FunI.XtX*FunI.beta_hat,1,function(z){z*FunI.beta_hat})
-  # if (! isSymmetric(FunI.PE.term3)){stop(print("Term.3 is not symmetric!"))}
-  #FunI.PE <- (cumsum(FunI.PE.term2) + diag(apply(apply(FunI.PE.term3,2,cumsum),1,cumsum)))/FunI.nv
   Y_hat=matrix(NA,nrow=dim(FunI.test.X)[1],ncol=dim(FunI.test.X)[2])
   for (ii in 1:dim(FunI.test.X)[1]){
     Y_hat[ii,]=cumsum(FunI.test.X[ii,]*FunI.beta_hat)
@@ -63,14 +54,10 @@ FunI.PEtilde<-function(FunI.XtY.tr, FunI.XtY.v, FunI.ntr, FunI.nv, FunI.Sigma,Fu
   if (! is.vector(FunI.XtY.tr)){stop(print("X_t_Y.tr should be a vector!"))}
   if (! is.vector(FunI.XtY.v)){stop(print("X_t_Y.v should be a vector!"))}
   if (length(FunI.XtY.tr)!=length(FunI.XtY.v)){stop(print("Please check dimensions of parameters!"))}
-  # if (is.matrix(FunI.Sigma) & !isSymmetric(FunI.Sigma)){stop(print("Sigma is not symmetric!"))}
   
   if(is.vector(FunI.Sigma)){
     FunI.beta_tilde <- (FunI.XtY.tr/FunI.ntr)/(FunI.Sigma)
-    #FunI.PE.term2 <- -2*FunI.beta_tilde*(FunI.XtY.v/FunI.nv)
     FunI.PE.term3 <- FunI.Sigma*(FunI.beta_tilde^2)
-    #FunI.PE <- cumsum(FunI.PE.term2+FunI.PE.term3)
-    #FunI.R2 <- -FunI.PE/FunI.Var.Y
     FunI.Var.Y.Hat = cumsum(FunI.PE.term3)
     FunI.Cov.Y_Y.Hat = cumsum(FunI.beta_tilde*(FunI.XtY.v/FunI.nv))
     FunI.Cor.squared = FunI.Cov.Y_Y.Hat^2/(FunI.Var.Y.Hat*FunI.Var.Y)
@@ -96,7 +83,6 @@ FunI.PEtilde<-function(FunI.XtY.tr, FunI.XtY.v, FunI.ntr, FunI.nv, FunI.Sigma,Fu
 #***    FunII.Ntr is a number for how large sample size is used to train, which is smaller than max(FunII.N.samplesize)
 #***    FunII.rep is a number for repeated-learning.
 
-#source("/Users/yupeilin/PRS-Fine-tuning/R/CVFunI_v4.R")
 
 FunII.TildeRL<-function(FunII.beta, FunII.SE, FunII.sigma, FunII.N.samplesize, FunII.Nv, FunII.rep, FunII.Corr=F){
   if(length(FunII.N.samplesize)==1){
@@ -124,12 +110,9 @@ FunII.TildeRL<-function(FunII.beta, FunII.SE, FunII.sigma, FunII.N.samplesize, F
   if (FunII.Corr==F){
     # If Corr=F, FunII.VarXY is a vector
     FunII.VarXY <- FunII.N.samplesize*(FunII.SE^2)*(FunII.sigma.diag^2)
-    #for (FunII.r in 1){
     for (FunII.r in 1:FunII.rep){
       FunII.XtY.train.temp <- rnorm(n=length(FunII.beta), mean = (FunII.Ntr/FunII.N.samplesize)*FunII.XtY, sd = sqrt((FunII.Ntr*FunII.Nv/FunII.N.samplesize)*FunII.VarXY))
-      #FunII.XtY.train.temp <- t(x.train)%*%y.train
       FunII.XtY.test.temp <- FunII.XtY - FunII.XtY.train.temp
-      #FunII.XtY.test.temp <- rnorm(n=length(FunII.beta), mean = (FunII.Nv/FunII.N.samplesize)*FunII.XtY, sd = sqrt((FunII.Ntr*FunII.Nv/FunII.N.samplesize)*FunII.VarXY))
       FunII.Zscore.temp <- FunII.XtY.train.temp/(sqrt(FunII.N.samplesize*FunII.Ntr)*FunII.SE*FunII.sigma.diag)
       FunII.order.temp <- order(abs(FunII.Zscore.temp),decreasing=T)
       if(is.vector(FunII.sigma)){
@@ -137,10 +120,6 @@ FunII.TildeRL<-function(FunII.beta, FunII.SE, FunII.sigma, FunII.N.samplesize, F
                                                         FunI.XtY.v=FunII.XtY.test.temp[FunII.order.temp],
                                                         FunI.ntr=FunII.Ntr[FunII.order.temp], FunI.nv=FunII.Nv,
                                                         FunI.Sigma=FunII.sigma.diag[FunII.order.temp],FunI.Var.Y=FunII.Var.Y)/FunII.rep
-        #FunII.PErr.Avg <- FunI.PEtilde(FunI.XtY.tr=FunII.XtY.train.temp[FunII.order.temp],
-        #FunI.XtY.v=FunII.XtY.test.temp[FunII.order.temp],
-        #FunI.ntr=FunII.Ntr, FunI.nv=FunII.Nv[FunII.order.temp],
-        #FunI.Sigma=FunII.sigma.diag[FunII.order.temp],FunI.Var.Y=FunII.Var.Y)
       }else{
         FunII.PErr.Avg <- FunII.PErr.Avg + FunI.PEtilde(FunI.XtY.tr=FunII.XtY.train.temp[FunII.order.temp],
                                                         FunI.XtY.v=FunII.XtY.test.temp[FunII.order.temp],
@@ -158,10 +137,6 @@ FunII.TildeRL<-function(FunII.beta, FunII.SE, FunII.sigma, FunII.N.samplesize, F
     if(is.vector(FunII.sigma)){
       #for (FunII.r in 1:FunII.rep){
       for (FunII.r in 1){
-        #FunII.PErr.Avg <- FunII.PErr.Avg + FunI.PEtilde(FunI.XtY.tr=FunII.XtY.train.temp[FunII.r, FunII.order.temp[,FunII.r]],
-        #FunI.XtY.v=FunII.XtY.test.temp[FunII.r, FunII.order.temp[,FunII.r]],
-        #FunI.ntr=FunII.Ntr, FunI.nv=FunII.Nv[FunII.order.temp[,FunII.r]],
-        #FunI.Sigma=FunII.sigma.diag[FunII.order.temp[,FunII.r]])/FunII.rep
         FunII.PErr.Avg <- FunI.PEtilde(FunI.XtY.tr=FunII.XtY.train.temp[FunII.r, FunII.order.temp[,FunII.r]],
                                        FunI.XtY.v=FunII.XtY.test.temp[FunII.r, FunII.order.temp[,FunII.r]],
                                        FunI.ntr=FunII.Ntr, FunI.nv=FunII.Nv[FunII.order.temp[,FunII.r]],
