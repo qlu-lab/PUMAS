1cbeb1f07404e029a7be111c29dd91caf8d44045
qlu-lab <33715415+qlu-lab@users.noreply.github.com>
2019-10-21 14:49:30 -0500

    Update pumas.R

--- R/pumas.R
+++ R/pumas.R
@@ -1,3 +1,28 @@
+pumas.main<-function(input_path, beta_header,maf_header,se_header, sample_size,flag_plot){
+  data.real <- read.table(paste0(input_path), header=T,na.strings=c("na","NA",""))
+  data.real=na.omit(data.real)
+  if (!is.character(beta_header)){stop(print("beta header should be a character"))}
+  if (!is.character(se_header)){stop(print("se header should be a character"))}
+  #TODO to create a MAF
+  if (is.null(maf_header)){print("MAF processing with reference")}
+  #If usr puts in a number for sample size
+  #if(is.numeric(sample_size)){N.sample = sample_size}
+  beta=data.real[,which(names(data.real)==beta_header)]
+  MAF=data.real[,which(names(data.real)==maf_header)]
+  se=data.real[,which(names(data.real)==se_header)]
+  X_VAR=2*MAF*(1-MAF)
+  N.sample = sample_size
+  
+  p.value=data.real$Pval
+  p.value=p.value[order(p.value,decreasing=F)]
+  
+  Res_TildeRL <- FunII.TildeRL(FunII.beta=beta,FunII.SE=se,FunII.sigma=X_VAR,FunII.N.samplesize=N.sample,FunII.Nv=0.25*N.sample,FunII.rep=4, FunII.Corr=F)
+  
+  if(flag_plot){FunI.plot(Res_TildeRL=Res_TildeRL,p.value=p.value)}
+  
+  
+}
+
 FunI.plot<-function(Res_TildeRL,p.value){
   png(paste0("/Users/yupeilin/pumas/result/T0030_pruned.png"),units="in",width = 9,height = 5,res = 300)
   plot(y=Res_TildeRL,x=log(p.value,base=10),main=paste0("T0030_pruned"),ylab="COR^2",xlab="log(P-value)")
