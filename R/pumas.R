16e40c8b7b62db4452d17cae31ed4045e7d5a892
qlu-lab <33715415+qlu-lab@users.noreply.github.com>
2019-10-23 13:39:31 -0500

    Update pumas.R
    resolve issue of not correctly reading p-value and adjust main title of the plot by input_path (need to refine this option later)

--- R/pumas.R
+++ R/pumas.R
@@ -1,20 +1,21 @@
-pumas.main<-function(input_path,output_path, beta_header,maf_header,se_header, sample_size,flag_plot){
+pumas.main<-function(input_path,output_path, beta_header,af_header,se_header,pvalue_header,sample_size,flag_plot){
   data.real <- read.table(paste0(input_path), header=T,na.strings=c("na","NA",""))
   data.real=na.omit(data.real)
   if (!is.character(beta_header)){stop(print("beta header should be a character"))}
   if (!is.character(se_header)){stop(print("se header should be a character"))}
+  if (!is.character(pvalue_header)){stop(print("p-value header should be a character"))}
   #TODO to create a MAF
-  if (is.null(maf_header)){print("MAF processing with reference")}
+  if (is.null(af_header)){print("Allele frequency is missing. Please provide the correct header for allele frequency.")}
   #If usr puts in a number for sample size
   #if(is.numeric(sample_size)){N.sample = sample_size}
   beta=data.real[,which(names(data.real)==beta_header)]
-  MAF=data.real[,which(names(data.real)==maf_header)]
+  AF=data.real[,which(names(data.real)==af_header)]
   se=data.real[,which(names(data.real)==se_header)]
-  X_VAR=2*MAF*(1-MAF)
+  pvalue=data.real[,which(names(data.real)==pvalue_header)]
+  X_VAR=2*AF*(1-AF)
   N.sample = sample_size
   
-  p.value=data.real$Pval
-  p.value=p.value[order(p.value,decreasing=F)]
+  pvalue=pvalue[order(pvalue,decreasing=F)]
   
   Res_TildeRL <- FunII.TildeRL(FunII.beta=beta,FunII.SE=se,FunII.sigma=X_VAR,FunII.N.samplesize=N.sample,FunII.Nv=0.25*N.sample,FunII.rep=4, FunII.Corr=F)
   
@@ -25,31 +26,16 @@ pumas.main<-function(input_path,output_path, beta_header,maf_header,se_header, s
 
 FunI.plot<-function(Res_TildeRL,p.value,output_path){
   png(paste0(output_path),units="in",width = 9,height = 5,res = 300)
-  plot(y=Res_TildeRL,x=log(p.value,base=10),main=paste0("T0030_pruned"),ylab="COR^2",xlab="log(P-value)")
+  plot(y=Res_TildeRL,x=log(p.value,base=10),main=paste0(input_path),ylab="R^2",xlab="log(P-value)")
   points(y=max(Res_TildeRL),x=log(p.value,base=10)[which(Res_TildeRL==max(Res_TildeRL))],col="red", pch=19)
   abline(v=log(p.value,base=10)[which(Res_TildeRL==max(Res_TildeRL))],col="red",lty=2)
-  text(labels=paste0(p.value[which(Res_TildeRL==max(Res_TildeRL))]),y=0,x=log(p.value,base=10)[which(Res_TildeRL==max(Res_TildeRL))],pos=3,col="red")
+  text(labels=paste0(p.value[which(Res_TildeRL==max(Res_TildeRL))]),y=0.5*max(Res_TildeRL),x=log(p.value,base=10)[which(Res_TildeRL==max(Res_TildeRL))],pos=3,col="red")
  # legend("p-value is : ", col="red", cex=0.8)
   dev.off()
 }
 ### ---  Funtion File ---###
 
 #--- Prediction Error: --->
-#------ the paramters should be ordered in advance
-FunI.PEhat<-function(FunI.beta_hat,FunI.test.Y,FunI.test.X){
-  if (! is.vector(FunI.beta_hat)){stop(print("beta_hat should be a vector!"))}
-  Y_hat=matrix(NA,nrow=dim(FunI.test.X)[1],ncol=dim(FunI.test.X)[2])
-  for (ii in 1:dim(FunI.test.X)[1]){
-    Y_hat[ii,]=cumsum(FunI.test.X[ii,]*FunI.beta_hat)
-  }
-  FunII.R2=c()
-  for (jj in 1:dim(Y_hat)[2]){
-    FunII.R2[jj] = (cor(Y_hat[,jj],FunI.test.Y))^2
-  }
-  return(FunII.R2)
-}
-
-
 #------ the paramters should be ordered in advance, including FunI.Sigma:
 #---------  *1. FunI.sigma is the true covariance matrix for (x_1,...,x_m), not necessary a diagnal matrix;
 FunI.PEtilde<-function(FunI.XtY.tr, FunI.XtY.v, FunI.ntr, FunI.nv, FunI.Sigma,FunI.Var.Y){
